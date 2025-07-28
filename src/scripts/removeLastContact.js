a1c2654f5f19a614009204df71f845c4841cbf01
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-28 12:42:32 +0300

    step 7-8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,23 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      console.log('The contact list is empty. There is nothing to delete.');
+      return;
+    }
+
+    const removed = contacts[contacts.length - 1];
+    contacts.pop();
+
+    await writeContacts(contacts);
+    console.log('Last contact deleted:', removed);
+  } catch (error) {
+    console.error('Error deleting last contact:', error.message);
+  }
+};
 
 removeLastContact();
