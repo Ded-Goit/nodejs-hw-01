1ad3299746bfc40ed0aa902d88c1b37161998bde
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-28 12:20:36 +0300

    step 1-3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,32 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    // Зчитуємо існуючі контакти
+    const existingContacts = await readContacts();
+
+    // Генеруємо нові контакти
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    // Об'єднуємо старі та нові контакти
+    const updatedContacts = [...existingContacts, ...newContacts];
+
+    // Записуємо оновлений список у файл
+    await writeContacts(updatedContacts);
+
+    console.log(
+      `Added successfully ${number} contacts. In total now: ${updatedContacts.length}`,
+    );
+  } catch (error) {
+    console.error(
+      'An error occurred while generating contacts:',
+      error.message,
+    );
+  }
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const jsonData = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, jsonData, 'utf-8');
+  } catch (error) {
+    console.error('Error writing to contacts file:', error.message);
+    throw error;
+  }
+};
